ebcc8f7ebbed4bca2489519086e6eebfcd01edcf
wlvlrtm <kimsk200012@gmail.com>
2021-09-15 11:23:44 +0900

    Update Python_VS_NoteBook.py

--- Python VS NoteBook/Python_VS_NoteBook.py
+++ Python VS NoteBook/Python_VS_NoteBook.py
@@ -1,4 +1,4 @@
 table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 8637678}
 
-print('Jack: {0[Jack]}; Sjoerd: {0[Sjoerd]}; '
-      'Dcab: {0[Dcab]}'.format(table))
+print('Jack: {0[Jack]}; Sjoerd: {1[Sjoerd]}; '
+      'Dcab: {2[Dcab]}'.format(table))
